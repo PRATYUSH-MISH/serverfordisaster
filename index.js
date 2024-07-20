15c9f663a48b0c28caeae765888267aa52acbc8d
PRATYUSH-MISH <pratoss32@gmail.com>
2024-07-20 15:27:10 +0530

    g

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: ' https://disaster-psi.vercel.app/',
+    methods: ['GET', 'POST'],
+}));
 app.use(bodyParser.json());
-const mongoUrl = 'mongodb://localhost:27017/Disaster';
 
-// Connect to MongoDB (replace 'your_database_url' with your actual MongoDB URL)
+const mongoUrl = 'mongodb://localhost:27017/Disaster';  // Replace with your MongoDB URL
+
+// Connect to MongoDB
 mongoose.connect(mongoUrl);
 
 // Define Emergency schema
